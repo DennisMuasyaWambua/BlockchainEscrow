7bd253d6cbbfbf99dc9268f7177a5701b39c0a7d
Dennis wambua <muasyathegreat4@gmail.com>
2025-02-12 09:27:09 +0300

    database url removed

--- blockchain_escrow_backend/settings.py
+++ blockchain_escrow_backend/settings.py
@@ -79,17 +79,17 @@ WSGI_APPLICATION = 'blockchain_escrow_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 CORS_ALLOW_ALL_ORIGINS = True
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_PUBLIC_URL"))
-}
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.getenv("DATABASE_PUBLIC_URL"))
+# }
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
