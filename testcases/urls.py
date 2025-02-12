4b9cbd02dfd1a6e63b9f2d641c512f05361f25fc
Dennis wambua <muasyathegreat4@gmail.com>
2025-02-12 15:10:31 +0300

    get repositories method added

--- testcases/urls.py
+++ testcases/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, CreateRepositoryView, GetRepositoryView
+from .views import RegisterView, LoginView, LogoutView, CreateRepositoryView, GetRepositories
 
 urlpatterns = [
     path('authentication/register/', RegisterView.as_view(), name='register'),
     path('authentication/login/', LoginView.as_view(), name="login"),
     path('authentication/logout/', LogoutView.as_view(), name = 'logout'),
     path('create-repo/', CreateRepositoryView.as_view(), name='github-auth'),
-    path('get-repos/', GetRepositoryView.as_view(), name='get-repos'),
+    path('get-repos/', GetRepositories.as_view(), name='get-repos'),
     ]
